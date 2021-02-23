7e6b1c89b493bf71042dfce45f0cb3edad32ba9d
Matthias Margush <matthias.margush@me.com>
2021-02-23 15:55:13 -0800

    faster start

--- emacs/.config/emacs/layers/ui.el
+++ emacs/.config/emacs/layers/ui.el
@@ -16,24 +16,7 @@
 
 (use-package editorconfig :config (editorconfig-mode))
 
-(use-package construction-paper-theme
-  :straight (construction-paper-theme :type git :host github :repo "matthias-margush/construction-paper-emacs")
 
-  :init
-  (defun me/modeline-style-line (&optional arg)
-    "Style the mode line a simple line."
-    (set-face-attribute 'mode-line nil :background nil :foreground nil :underline t :box nil)
-    (set-face-attribute 'mode-line nil :inherit 'default :underline t)
-    (set-face-attribute 'mode-line-inactive nil :background nil :foreground nil :underline t :box nil)
-    (set-face-attribute 'mode-line-inactive nil :inherit 'default :underline t))
-
-  (setq-default mode-line-format '(""))
-  (advice-add 'enable-theme :after #'me/modeline-style-line)
-  (add-to-list 'after-make-frame-functions #'me/modeline-style-line)
-
-  :config
-  (require 'construction-paper-theme)
-  (construction-paper-theme-light))
 
 (use-package default-text-scale
   :bind (("s-=" . default-text-scale-increase)
