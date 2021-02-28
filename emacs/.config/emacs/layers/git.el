dca2e4a8992f3521e4bd2e73a3b64bb7a1196bb1
Matthias Margush <matthias.margush@me.com>
2021-02-27 16:18:24 -0800

    git-link selection

--- emacs/.config/emacs/layers/git.el
+++ emacs/.config/emacs/layers/git.el
@@ -1,21 +1,24 @@
 (use-package magit
-    :commands (magit-status magit-blame-addition git-link)
-    :general (:states '(normal) :prefix leader "g" git-map)
-    :bind (:map git-map
-                ("g" . magit-status)
-                ("b" . magit-blame-addition))
-    :init
-    (setq git-map (make-sparse-keymap))
-    (remove-hook 'magit-section-highlight-hook #'magit-section-highlight)
-    (remove-hook 'magit-section-highlight-hook #'magit-diff-highlight)
-    (setq magit-display-buffer-function 'magit-display-buffer-fullframe-status-v1
-          magit-diff-refine-hunk 'all))
+  :commands (magit-status magit-blame-addition git-link)
+  :general (:states '(normal) :prefix leader "g" git-map)
+  :bind (:map git-map
+              ("g" . magit-status)
+              ("b" . magit-blame-addition))
+  :init
+  (setq git-map (make-sparse-keymap))
+  (remove-hook 'magit-section-highlight-hook #'magit-section-highlight)
+  (remove-hook 'magit-section-highlight-hook #'magit-diff-highlight)
+  (setq magit-display-buffer-function 'magit-display-buffer-fullframe-status-v1
+        magit-diff-refine-hunk 'all))
 
 (use-package forge :after markdown)
 
 (use-package git-link
-    :bind (:map git-map
-                ("l" . git-link)))
+  :general
+  (:states 'visual :prefix leader "gl" #'git-link)
+
+  :bind
+  (:map git-map ("l" . git-link)))
 
 (use-package diff-hl
   :general
