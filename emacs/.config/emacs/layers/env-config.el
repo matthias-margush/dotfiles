e8f653e29e482fca308eca6ca2a3e304ac6cb03f
Matthias Margush <matthias.margush@me.com>
2021-03-07 13:23:40 -0800

    env

---
+++ emacs/.config/emacs/layers/env-config.el
@@ -0,0 +1,15 @@
+;; -*- mode: emacs-lisp; lexical-binding: t; -*-
+
+(require 'package-config)
+
+(setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
+(setq exec-path (append exec-path '("/usr/local/bin")))
+
+(use-package envrc
+  :config (envrc-global-mode))
+
+(use-package exec-path-from-shell
+  :demand t
+  :config (exec-path-from-shell-initialize))
+
+(provide 'env-config)
