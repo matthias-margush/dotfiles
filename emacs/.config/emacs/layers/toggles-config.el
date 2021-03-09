e823633e5b1cb2ae02e243ce97669eddb455ecaf
Matthias Margush <matthias.margush@me.com>
2021-03-08 20:16:02 -0800

    debug on error toggle

--- emacs/.config/emacs/layers/toggles-config.el
+++ emacs/.config/emacs/layers/toggles-config.el
@@ -7,6 +7,7 @@
 (define-key me/toggles-map (kbd "r") #'rainbow-mode)
 (define-key me/toggles-map (kbd "v") #'visible-mode)
 (define-key me/toggles-map (kbd "w") #'whitespace-mode)
+(define-key me/toggles-map (kbd "d") #'toggle-debug-on-error)
 
 (general-define-key :states 'normal :prefix leader "t" me/toggles-map)
 
