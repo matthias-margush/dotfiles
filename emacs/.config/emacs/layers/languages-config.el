abd2c263747f9a599b2e725677bb885588651f91
Matthias Margush <matthias.margush@me.com>
2021-03-10 00:01:23 -0800

    fix format and organize imports

--- emacs/.config/emacs/layers/languages-config.el
+++ emacs/.config/emacs/layers/languages-config.el
@@ -52,8 +52,8 @@
   :init
   (defun me/go-editor-settings ()
     ;; (setq flycheck-checker 'gometalinter)
-    (add-hook 'before-save-hook #'eglot-format-buffer -10 t)
-    (add-hook 'before-save-hook #'eglot-organize-imports t t)
+    (add-hook 'before-save-hook #'lsp-organize-imports t t)
+    (add-hook 'before-save-hook #'lsp-format-buffer -10 t)
     (electric-pair-mode))
 
   (eval-after-load "go-mode" '(setq go-mode-map (make-sparse-keymap))))
