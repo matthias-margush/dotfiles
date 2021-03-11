cca9d61665a10c2900e6f4cf71a3dad157db4acf
Matthias Margush <matthias.margush@me.com>
2021-03-11 00:04:05 -0800

    completion

--- emacs/.config/emacs/layers/completions-config.el
+++ emacs/.config/emacs/layers/completions-config.el
@@ -1,6 +1,7 @@
 ;; -*- mode: emacs-lisp; lexical-binding: t; -*-
 
 (require 'package-config)
+(require 'evil-config)
 
 (use-package company
   :demand t
@@ -9,6 +10,11 @@
   (setq company-selection-wrap-around t)
 
   :config
+  (define-key evil-insert-state-map (kbd "C-n") nil)
+  (evil-define-key 'insert global-map (kbd "C-SPC") #'company-complete)
+
+  (define-key company-active-map (kbd "C-n") #'company-select-next)
+  (define-key company-active-map (kbd "C-p") #'company-select-previous)
   (global-company-mode))
 
 (provide 'completions-config)
