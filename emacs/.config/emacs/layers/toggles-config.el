2ce499553300403502bb2f442aaf330a601197a4
Matthias Margush <matthias.margush@me.com>
2021-03-12 20:25:17 -0800

    toggles

--- emacs/.config/emacs/layers/toggles-config.el
+++ emacs/.config/emacs/layers/toggles-config.el
@@ -8,6 +8,8 @@
 (define-key me/toggles-map (kbd "v") #'visible-mode)
 (define-key me/toggles-map (kbd "w") #'whitespace-mode)
 (define-key me/toggles-map (kbd "d") #'toggle-debug-on-error)
+(define-key me/toggles-map (kbd "p") #'orgraphy-mode)
+(define-key me/toggles-map (kbd "f") #'font-lock-mode)
 
 (general-define-key :states 'normal :prefix leader "t" me/toggles-map)
 
