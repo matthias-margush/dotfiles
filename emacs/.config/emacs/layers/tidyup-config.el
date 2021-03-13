c86f7838bf470c645b8a3d0ee2d4e11c5d7a7415
Matthias Margush <matthias.margush@me.com>
2021-03-12 19:44:48 -0800

    tidy

--- emacs/.config/emacs/layers/tidyup-config.el
+++ emacs/.config/emacs/layers/tidyup-config.el
@@ -19,6 +19,7 @@
 ;; Suppress "Cleaning up the recentf...done (0 removed)"
 (advice-add 'recentf-cleanup :around #'me/suppress-messages)
 (advice-add 'recentf-load-list :around #'me/suppress-messages)
+(advice-add 'tree-sitter-langs-install-grammars :around #'me/suppress-messages)
 
 (defun display-startup-echo-area-message ()
   "Redefine to clean up startup echo area message."
@@ -30,8 +31,12 @@
 ;; Disable text wrapping generally
 (setq-default truncate-lines t)
 
-;;;  Literal noise
+;;  Literal noise
 (setq ring-bell-function 'ignore)
+
+;; help with big files
+(setq bidi-paragraph-direction 'left-to-right)
+(setq bidi-inhibit-bpa t)
 
 (setq mac-option-modifier 'meta)
 
