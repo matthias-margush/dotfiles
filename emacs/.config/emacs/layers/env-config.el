279bc17be8f66ce7019df2f0824793ac03465fc3
Matthias Margush <matthias.margush@me.com>
2021-03-16 23:52:59 -0700

    add paths

--- emacs/.config/emacs/layers/env-config.el
+++ emacs/.config/emacs/layers/env-config.el
@@ -5,11 +5,13 @@
 (setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
 (setq exec-path (append exec-path '("/usr/local/bin")))
 
+(setenv "PATH" (concat (getenv "PATH") ":" (expand-file-name "~/bin")))
+(setq exec-path (append exec-path `(,(expand-file-name "~/bin"))))
+
+(setenv "PATH" (concat (getenv "PATH") ":" (expand-file-name "~/go/bin")))
+(setq exec-path (append exec-path `(,(expand-file-name "~/go/bin"))))
+
 (use-package envrc
   :config (envrc-global-mode))
-
-;; (use-package exec-path-from-shell
-;;   :demand t
-;;   :config (exec-path-from-shell-initialize))
 
 (provide 'env-config)
