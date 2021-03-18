b8d5a53ae31e7011ca98715a9fba3b001c278bab
Matthias Margush <matthias.margush@me.com>
2021-03-17 22:23:06 -0700

    toggles

--- emacs/.config/emacs/layers/toggles-config.el
+++ emacs/.config/emacs/layers/toggles-config.el
@@ -6,10 +6,13 @@
 
 (define-key me/toggles-map (kbd "r") #'rainbow-mode)
 (define-key me/toggles-map (kbd "v") #'visible-mode)
-(define-key me/toggles-map (kbd "w") #'whitespace-mode)
+(define-key me/toggles-map (kbd "w") #'toggle-word-wrap)
+(define-key me/toggles-map (kbd "W") #'whitespace-mode)
 (define-key me/toggles-map (kbd "d") #'toggle-debug-on-error)
 (define-key me/toggles-map (kbd "p") #'orgraphy-mode)
 (define-key me/toggles-map (kbd "f") #'font-lock-mode)
+(define-key me/toggles-map (kbd "m") #'smerge-mode)
+(define-key me/toggles-map (kbd "t") #'toggle-truncate-lines)
 
 (general-define-key :states 'normal :prefix leader "t" me/toggles-map)
 
