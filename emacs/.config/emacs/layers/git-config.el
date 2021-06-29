0e14f38b51ba8d0609f6374cc07f21e4f3d11d2a
Matthias Margush <matthias.margush@gmail.com>
2021-06-28 18:18:47 -0700

    forge

--- emacs/.config/emacs/layers/git-config.el
+++ emacs/.config/emacs/layers/git-config.el
@@ -5,15 +5,22 @@
 (setq vc-follow-symlinks t)
 (setq ediff-window-setup-function #'ediff-setup-windows-plain)
 
+(use-package pinentry
+  :config
+  (pinentry-start))
+
 (use-package magit
-  :defer 3
+  :demand t
   :commands (magit-status magit-blame-addition)
 
   :general
   (:keymaps 'magit-mode-map "SPC" nil)  ; magit overrides leader
+  (:states 'normal :prefix leader "g" git-map)
+  (:keymaps 'magit-mode-map :states 'normal :prefix local-leader
+            "r" #'github-review-forge-pr-at-point)
+  
   (:keymaps 'with-editor-mode-map :states 'normal ",cc" #'with-editor-finish)
   (:keymaps 'with-editor-mode-map :states 'normal ",ck" #'with-editor-cancel)
-  (:states 'normal :prefix leader "g" git-map)
 
   ;;  smerge
   (:keymaps 'smerge-mode-map :states 'normal "]n" #'smerge-next)
@@ -25,22 +32,42 @@
   (:keymaps 'smerge-mode-map :prefix local-leader "a" #'smerge-keep-all)
 
   (:keymaps 'git-map
-    "g" #'magit-status
-    "b" #'magit-blame-addition)
+            "g" #'magit-status
+            "b" #'magit-blame-addition)
 
   :init
+  (defun me/worktree (branch start-point &optional force)
+    ""
+    (interactive
+     `(,@(magit-branch-read-args "Create and checkout branch")
+       ,current-prefix-arg))
+    (interactive)
+    (let ((path (expand-file-name (concat (me/project-root) "../" branch))))
+      (magit-worktree-branch path branch start-point force)
+      (me/project-notes)))
+
   (setq git-map (make-sparse-keymap))
   (remove-hook 'magit-section-highlight-hook #'magit-section-highlight)
   (remove-hook 'magit-section-highlight-hook #'magit-diff-highlight)
   (setq magit-display-buffer-function 'magit-display-buffer-fullframe-status-v1
-    magit-diff-refine-hunk 'all))
+        magit-diff-refine-hunk 'all))
 
-(use-package forge :after markdown)
+(use-package forge :after magit
+  :init
+  (setq magit-filenotify-mode nil
+        magit-refresh-status-buffer nil
+        forge-pull-notifications nil)
+
+  :config
+  (emacsql (forge-db) "PRAGMA journal_mode=WAL")
+  (emacsql (forge-db) "PRAGMA synchronous=OFF"))
+
+(use-package github-review)
 
 (use-package git-link
   :commands (git-link)
   :general
-  (:states 'visual :prefix leader "gl" #'git-link)
+  (:states 'visual :map 'git-map  "gl" #'git-link)
 
   :bind
   (:map git-map ("l" . git-link)))
@@ -49,18 +76,18 @@
   :defer 3
   :general
   (:keymaps 'normal
-    "]g" 'diff-hl-next-hunk
-    "[g" 'diff-hl-previous-hunk
-    "gh" 'diff-hl-diff-goto-hunk)
+            "]g" 'diff-hl-next-hunk
+            "[g" 'diff-hl-previous-hunk
+            "gh" 'diff-hl-diff-goto-hunk)
 
   :hook
   ((magit-pre-refresh . diff-hl-magit-pre-refresh)
-    (magit-post-refresh . diff-hl-magit-post-refresh)
-    (dired-mode . diff-hl-dired-mode))
+   (magit-post-refresh . diff-hl-magit-post-refresh)
+   (dired-mode . diff-hl-dired-mode))
 
   :init
   (setq diff-hl-draw-borders nil
-    diff-hl-side 'right)
+        diff-hl-side 'right)
 
   :config
   (global-diff-hl-mode)
